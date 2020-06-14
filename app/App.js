5022b13aa2c15d2b26fc11120b2706fdda041c1f
Leo Lin <mml0816@gmail.com>
2020-06-13 20:40:23 -0700

    add documentation

--- app/App.js
+++ app/App.js
@@ -108,7 +108,6 @@ export default class App extends Component {
           <ScrollView
             contentInsetAdjustmentBehavior="automatic"
             style={styles.scrollView}>
-            <Header />
             {global.HermesInternal == null ? null : (
               <View style={styles.engine}>
                 <Text style={styles.footer}>Engine: Hermes</Text>
