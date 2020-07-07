043275b3b6843b8c7fc24d715f2ba6fa4c54b067
Leo Lin <mml0816@gmail.com>
2020-07-07 11:48:17 -0700

    add device info

--- app/App.js
+++ app/App.js
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import React, {Component} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,63 +6,125 @@ import {
   View,
   Text,
   StatusBar,
+  Button,
+  Alert
 } from 'react-native';
 
 import {
   Header,
-  LearnMoreLinks,
   Colors,
-  DebugInstructions,
-  ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
+import DeviceInfo from 'react-native-device-info';
+// import Geolocation from 'react-native-geolocation-service';
+
+// var moment = require('moment');
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      macAddress: ""
+    };
+  }
+
+  componentDidMount() {
+    DeviceInfo.getMacAddress().then(mac => {
+      macAddress = JSON.stringify(mac);
+      this.setState({macAddress : JSON.stringify(mac)});
+    })
+
+    // var watchID = Geolocation.watchPosition(
+    //   position => {
+    //     const {latitude, longitude} = position.coords;
+
+    //     this.setState({
+    //       latitude: latitude,
+    //       longitude: longitude,
+    //       inBound: perimeterContains(latitude, longitude),
+    //       timestamp: position.timestamp
+    //     })
+    //   },
+    //   error => console.log(error),
+    //   {
+    //     enableHighAccuracy: true,
+    //     timeout: 20000,
+    //     distanceFilter: 1
+    //   }
+    // )
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // if (!prevState.inBound && this.state.inBound) {
+    //   Alert.alert(
+    //     "Company Entry",
+    //     "You are now entering company area. Your device information will be synced with your user information in the company's database",
+    //     [
+    //       {
+    //         text: "OK", onPress: () => this.sendDeviceInfo()
+    //       }
+    //     ],
+    //     { cancelable: false }
+    //   )
+    // } else if (prevState.inBound && !this.state.inBound) {
+    //   Alert.alert(
+    //     "Company Exit",
+    //     "You are now exiting company area. Your device information will no longer be synced.",
+    //     [
+    //       {
+    //         text: "OK", onPress: () => this.sendExit()
+    //       }
+    //     ],
+    //     { cancelable: false }
+    //   )
+    // }
+  }
+
+  // Send device information to the server
+  sendDeviceInfo() {
+    console.log("sent!");
+  }
+
+  // Send exit message to the server
+  sendExit() {
+    console.log("exit!");
+  }
+
+  render() {
+    // var latitude = this.state.latitude;
+    // var longitude = this.state.longitude;
+    var macAddress = this.state.macAddress;
+    var deviceID = DeviceInfo.getUniqueId();
+
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Device Information</Text>
+                <Text style={styles.sectionDescription}>
+                  Mac Address: {macAddress}
+                </Text>
+                <Text style={styles.sectionDescription}>
+                  Unique Device ID: {deviceID}
+                </Text>
+              </View>
             </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   scrollView: {
@@ -111,4 +165,52 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+// Helper Function: Check if perimeter contains point
+// const PERIMETER = require('./perimeter.json');
+// function perimeterContains(lat, lng) {
+//   var target = {lat: lat, lng: lng};
+//   var intersections = 0;
+
+//   for (var i = 0; i < PERIMETER.length; i++) {
+//     var a = PERIMETER[i],
+//         j = i+1;
+//     if (j >= PERIMETER.length) j = 0;
+//     var b = PERIMETER[j];
+//     if (crossSegment(target, a, b)) intersections++;
+//   }
+
+//   return (intersections % 2 == 1);
+
+//   function crossSegment(point, a, b) {
+//     var px = point.lng,
+//             py = point.lat,
+//             ax = a.lng,
+//             ay = a.lat,
+//             bx = b.lng,
+//             by = b.lat;
+//         if (ay > by) {
+//             ax = b.lng;
+//             ay = b.lat;
+//             bx = a.lng;
+//             by = a.lat;
+//         }
+        
+//         if (px < 0) {
+//             px += 360;
+//         }
+//         if (ax < 0) {
+//             ax += 360;
+//         }
+//         if (bx < 0) {
+//             bx += 360;
+//         }
+
+//         if (py == ay || py == by) py += 0.00000001;
+//         if ((py > by || py < ay) || (px > Math.max(ax, bx))) return false;
+//         if (px < Math.min(ax, bx)) return true;
+
+//         var red = (ax != bx) ? ((by - ay) / (bx - ax)) : Infinity;
+//         var blue = (ax != px) ? ((py - ay) / (px - ax)) : Infinity;
+//         return (blue >= red)
+//   }
+// }
